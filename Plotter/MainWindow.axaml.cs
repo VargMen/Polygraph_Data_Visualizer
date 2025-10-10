63e2ab625ebff57830221df8d1c7bbd575061842
VargMen <asusa0184@gmail.com>
2025-10-10 19:56:18 +0300

    Implement multiple plots drawing

--- Plotter/MainWindow.axaml.cs
+++ Plotter/MainWindow.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia.Controls;
 using Avalonia.Threading;
+using ScottPlot;
 using ScottPlot.Avalonia;
 using System;
 using System.Collections.Generic;
@@ -15,7 +16,10 @@ namespace Plotter
         private readonly Timer _timer = new(50) { AutoReset = true, Enabled = false };
 
         private readonly List<double> _time = new List<double>();
-        private readonly List<double> _valueYs = new List<double>();
+        private static readonly uint _plotsAmount = 20;
+        private readonly List<double>[] _ys = new List<double>[_plotsAmount];
+        private readonly double _yOffsetScale = 5.0;
+        private double[] _yOffsets = new double[_plotsAmount];
 
         public MainWindow()
         {
@@ -29,8 +33,15 @@ namespace Plotter
 
             AvaPlot1.Plot.Layout.Frameless();
 
-
-            AvaPlot1.Plot.Add.Scatter(_time, _valueYs);
+            for (int i = 0; i < _plotsAmount; i++)
+            {
+                _ys[i] = new List<double>();
+                _yOffsets[i] = i * _yOffsetScale;
+                var s = AvaPlot1.Plot.Add.Scatter(_time, _ys[i], color: ScottPlot.Color.FromHex(RandomHexRgb()));
+                s.MarkerSize = 0;
+                s.LineWidth = 2;
+                s.LinePattern = ScottPlot.LinePattern.Solid;
+            }
 
             SetupTimer();
         }
@@ -38,25 +49,39 @@ namespace Plotter
         private void SetupTimer()
         {
             _timer.Elapsed += OnTick;
+            _timer.Interval = 50;
             _timer.Start();
         }
 
         private void OnTick(object? s, ElapsedEventArgs e)
         {
             double t = _stopwatch.Elapsed.TotalSeconds;
-            double y = GetCurrentValue(t);
+
+            // generate 10 different signals
+            double[] vals = new double[_plotsAmount];
+            for (int i = 0; i < vals.Length; i++)
+            {
+                // vary frequency/phase/amplitude a bit per series
+                double freq = 0.6 + i * 0.12;
+                double amp = 20 + i * 3;
+                double ph = i * 0.4;
+                vals[i] = 50 + amp * Math.Sin(t * freq + ph);
+            }
 
             lock (AvaPlot1.Plot.Sync)
             {
                 _time.Add(t);
-                _valueYs.Add(y);
+                for (int i = 0; i < _plotsAmount; i++)
+                    _ys[i].Add(vals[i] + _yOffsets[i] * 15);
             }
 
             Dispatcher.UIThread.Post(() => AvaPlot1.Refresh());
         }
-        private double GetCurrentValue(double time)
+        public static string RandomHexRgb()
         {
-            return 50 + 50 * Math.Sin(time / 5.0) + (time * 0.5);
+            var rng = Random.Shared;
+            int r = rng.Next(256), g = rng.Next(256), b = rng.Next(256);
+            return $"#{r:X2}{g:X2}{b:X2}";
         }
     }
 }
