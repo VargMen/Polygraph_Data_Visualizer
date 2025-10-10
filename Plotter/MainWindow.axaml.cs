0b443b47f29e1dfb0d059a67d91fc60ebc032981
VargMen <asusa0184@gmail.com>
2025-10-10 18:30:59 +0300

    Create Plotter project. Draw dynamic plot with timer

---
+++ Plotter/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Plotter
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
